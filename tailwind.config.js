4520a255a0d9ebea829c6c07761810ead0217382
Brahamjeet <113265469+impulsiveblast@users.noreply.github.com>
2025-10-26 14:42:39 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        softbeige: '#efe3db',
+        card: '#f3e8e3',
+        sidebar: '#0b0b0b'
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
